580c6effea8010b8ceeea58ef9b69053d712e53e
darlison-calm <bernardo.darlison@gmail.com>
2025-04-22 23:45:39 -0300

    feat: remake sign up

--- src/components/ui/inputField.tsx
+++ src/components/ui/inputField.tsx
@@ -1,27 +1,30 @@
 import { InputFieldProps } from "@/types/formTypes";
-import { TCreateUserSchema } from "@/users/types/userTypes";
-
+import { TCreateUserSchema} from "@/users/types/userTypes";
 
 export default function InputField({ id, name, type, label, register, errors }: InputFieldProps<TCreateUserSchema>) {
-    return (
-      <div className="space-y-1">
-        <label
-          htmlFor={id}
-          className="block text-sm text-black-500 font-semibold"
-        >
-          {label}
-        </label>
+  return (
+    <div className="relative">
+      <div className="relative z-0">
         <input
           {...register(name)}
           type={type}
           id={id}
-          className="block px-2 py-2 w-full text-sm text-gray-900 bg-transparent rounded-md border border-gray-300 appearance-none focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
+          className={`block px-3 py-2.5 w-full text-sm text-gray-900 bg-transparent rounded-md border appearance-none focus:outline-none focus:ring-1 peer dark:text-white dark:border-gray-500 placeholder-transparent`}
+          placeholder=" "
         />
-        {errors[name] && (
-          <p className="text-sm text-red-600">
-            {errors[name]?.message}
-          </p>
-        )}
+        <label
+          htmlFor={id}
+          className={`absolute text-sm duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2'text-gray-500 peer-focus:text-blue-500'
+          peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-85 peer-focus:-translate-y-4 left-1 dark:bg-gray-900 dark:text-gray-400`}
+        >
+          {label}
+        </label>
       </div>
-    );
+      {errors[name] && (
+        <p className="mt-2 text-sm text-red-600 dark:text-red-400">
+          <span></span> {errors[name]?.message}
+        </p>
+      )}
+    </div>
+  );
 }
